220e72274ebfce3d1955cb37bec7576bd59077d7
root <root@DESKTOP-4IPI7EQ>
2023-07-08 01:46:07 +0500

    newcommit

---
+++ app.js
@@ -0,0 +1,30 @@
+// print statement
+// number without inverted commas
+console.log(525);
+// letter with inverted commas
+console.log("hello world");
+
+// datatypes
+
+// primitive
+//  which can be identified easily
+// number 12345
+// string asdf
+// boolean true/false
+
+// non primitive
+//  which can not be identified easily
+// array [1234, true 'aaaa']
+// Object { name : "ahad", age:11}
+
+
+
+
+
+// variables
+
+// var
+// let
+// const
+//  var name = "ahad";  old way
+// let
